0d1ecc8dba89fe6d124a7d435d3c07374c10ca80
Christina Delligatta <tinadelli@MacBook-Pro.local>
2020-07-20 22:05:37 -0400

    positively mvp

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,22 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    #create a dictionary
+    hash_table = {}
+    #create the array of results
+    result = []
 
+    #add positive numbers to our hash table
+    for num in a:
+        if num >= 0:
+            hash_table[num] = num 
+
+    #check if our positive number in our hash table has a negative value
+    for num in a:
+        if num < 0 and -num in hash_table:
+            result.append(-num)
     return result
+
 
 
 if __name__ == "__main__":
