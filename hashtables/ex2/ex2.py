a51899db06ab333c79469ee5d7d6395dfbe5f1f8
Christina Delligatta <tinadelli@MacBook-Pro.local>
2020-07-20 20:01:46 -0400

    reconstruct trip completed

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,27 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    #create a dictionary
+    hash_table = {}
+    #create array with None
+    route = [None] * length
+    #create key value pair
+    #iterate through array
+    for i in range(length):
+        #add the destinations into our hash table 
+        hash_table[tickets[i].source] = tickets[i].destination 
+        #if the current ticket index has a source of None
+        if tickets[i].source == "NONE" and route[0] == None:
+            route[0] = tickets[i].destination #this will be our first flight
+
+    # now we can check the value of the starting location and go from there to construct the entire route 
+    
+    #iterate through array
+    for ticket in range(length):
+        #if the ticket isn't the first and the last index is not none
+        if ticket > 0 and route[ticket - 1] is not None:
+            #set the destination to the ticket
+            route[ticket] = hash_table[route[ticket - 1]]
+
 
     return route
